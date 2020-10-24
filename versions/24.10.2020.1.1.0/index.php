5fb09625e5a60e21ab3bd703e0e157e8a7d81c34
Maurice Keightley <maurice.keightley@gmail.com>
2020-10-24 11:04:05 +1000

    post

--- versions/24.10.2020.1.1.0/index.php
+++ versions/24.10.2020.1.1.0/index.php
@@ -33,5 +33,12 @@
 		print_r($_SESSION);
 	echo '</pre>';
 */
-	echo json_encode($_POST['test']);
+
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+
+	echo $json;
 ?>
