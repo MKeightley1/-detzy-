4007ca52b987d928a6286ccede3fab206f282f3e
Maurice Keightley <maurice.keightley@gmail.com>
2020-10-24 11:34:39 +1000

    demo

--- versions/24.10.2020.1.1.0/index.php
+++ versions/24.10.2020.1.1.0/index.php
@@ -3,6 +3,7 @@
 		sleep(5);		
 		return true;	
 	}	
+	
 	function sendEmail(){
 		ini_set( 'display_errors', 1 );
 		error_reporting( E_ALL );
@@ -13,6 +14,35 @@
 		$headers = "From:" . $from;
   		return mail($to,$subject,$message, $headers);	
 	}
+	
+	function asyncURLReader($listOfUrls) {
+		
+		$multiCurl = [];
+		$result = [];
+	
+		$mh = curl_multi_init();
+		foreach ($listOfUrls as $i => $url) {
+		  $multiCurl[$i] = curl_init();
+		  curl_setopt($multiCurl[$i], CURLOPT_URL,$url);
+		  curl_setopt($multiCurl[$i], CURLOPT_HEADER,0);
+		  curl_setopt($multiCurl[$i], CURLOPT_RETURNTRANSFER,1);
+		  curl_multi_add_handle($mh, $multiCurl[$i]);
+		}
+		$index=null;
+		do {
+		  curl_multi_exec($mh,$index);
+		} while($index > 0);
+	
+		foreach($multiCurl as $k => $ch) {
+		  $result[$k] = curl_multi_getcontent($ch);
+
+		  curl_multi_remove_handle($mh, $ch);
+		}
+		curl_multi_close($mh);
+		
+		return $result;
+	}
+	
 	function gateway( $functionName ){
 		$startTime = date_timestamp_get(date_create());
  		$result = $functionName();
@@ -25,20 +55,65 @@
 			"output" => $result		
 		];				
 		$_SESSION["logHistory"][] = $record;
+		
+		return $result;
 	}	
 	
-	//gateway( 'sendEmail' );	
+	function recievePostData(){
+		// Takes raw data from the request
+		$postDataJson = file_get_contents('php://input');
+
+		// Converts it into a PHP object
+		$data = json_decode($postDataJson);
+	
+		$_SESSION['data']['post'] = $data;
+		
+		return true;
+	}
+	
+	function transformer(){
+		$listOfUrls = $_SESSION['data']['post']['urls'];
+		
+		// seek url page contents
+		$pageContents = asyncURLReader($listOfUrls);
+		
+		// for each page contents retrieved
+		$sumCount = count($pageContents);
+		
+		$results = [];
+		for($i=0;$i<$sumCount;$i++){
+			// for each regex filter identified
+			foreach ($_SESSION['data']['post']['Regex'] as $ref => $regex){
+				preg_match_all($regex, $pageContents[$i], $regexData);
+		
+				if($regexData[1] && $regexData[1]!== NULL){
+					$results[] = $regexData[1];
+				}
+			}
+		}
+		
+		$_SESSION['data']['response']['urls']=$result;
+		return true;
+	}
+	
+	
+	//acknowledge php input
+	gateway( 'recievePostData' );	
+	
+	//call async curl reader
+	gateway( 'transformer' );
+	
+	
 	/*
 	echo '<pre>';
 		print_r($_SESSION);
 	echo '</pre>';
 */
 
-// Takes raw data from the request
-$json = file_get_contents('php://input');
 
-// Converts it into a PHP object
-$data = json_decode($json);
+	$response['data']['session']=$_SESSION;
 
-	echo $json;
+
+
+	echo json_encode($response);
 ?>
