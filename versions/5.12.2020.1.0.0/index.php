83e79d98ab5ce190ef1b053d33b0404568d8b739
Maurice Keightley <maurice.keightley@gmail.com>
2020-12-05 15:24:02 +1000

    collapsed all

--- versions/5.12.2020.1.0.0/index.php
+++ versions/5.12.2020.1.0.0/index.php
@@ -1,40 +1,50 @@
 <?php	
-	$regex = [
-      'break' 	=> '/(This\ shop\ is\ taking\ a\ short\ break)/',
-      'sold'	=> '/(Sorry\,\ this\ item\ is\ sold\ out)/',
-      'unavailable' => '/(Sorry\,\ this\ item\ is\ unavailable)/'
-    ];
+	// define constants
+	defineConstants();
+	// define settings
+	registerGetParameters();
+	// define post data
+	definePostData();
+	//collect special Url
+	deConverter($postDataData);
 	
-	$postDataData =  [[
-		'meta_value'=>'https://www.awin1.com/cread.php?awinmid=10781&awinaffid=338915&clickref=&ued=https%3A%2F%2Fwww.etsy.com%2Fau%2Flisting%2F744470061%2Fquokkan-around-the-christmas-tree',
-		'post_title'=>'Quokkan Around the Christmas Tree Greeting Card',
-		'post_status'=>'publish',
-		'guid'=>'https://aussiemadechristmas.com.au/product/quokkan-around-the-christmas-tree/'
-	]];
-
-
-	//collect GET data
-	$flagRegister = isset($_GET['register'])?$_GET['register'] : '0';
-	//collect php post data
-	if((int)$flagRegister[0]){
-		$postDataData = json_decode($_POST);
-	}
-	$postDataData = deConverter($postDataData);
-	$_SESSION['data']['post']=$postDataData;
 	echo json_encode($_SESSION);
 	
-
-
+	function defineConstants(){
+		$_SESSION['data']['regex'] = [
+		  'break' 	=> '/(This\ shop\ is\ taking\ a\ short\ break)/',
+		  'sold'	=> '/(Sorry\,\ this\ item\ is\ sold\ out)/',
+		  'unavailable' => '/(Sorry\,\ this\ item\ is\ unavailable)/'
+		];
+		$_SESSION['data']['post'] =  [[
+			'meta_value'=>'https://www.awin1.com/cread.php?awinmid=10781&awinaffid=338915&clickref=&ued=https%3A%2F%2Fwww.etsy.com%2Fau%2Flisting%2F744470061%2Fquokkan-around-the-christmas-tree',
+			'post_title'=>'Quokkan Around the Christmas Tree Greeting Card',
+			'post_status'=>'publish',
+			'guid'=>'https://aussiemadechristmas.com.au/product/quokkan-around-the-christmas-tree/'
+		]];
+	}
+	
+	function definePostData(){
+		//collect php post data
+		if((int)$_SESSION['data']['urlParameters'][0]){
+			$_SESSION['data']['post'] = json_decode($_POST);
+		}
+	}
+	
+	function registerGetParameters(){
+		$_SESSION['data']['urlParameters'] = isset($_GET['register'])?$_GET['register'] : '0';
+	}
+	
 	function deConverter($data){
+		$listOfUrls = [];
 		foreach($data as $key => $value){
 			$tempString = $value['meta_value'];
 			$tempString = str_replace("%2F","/",$tempString);
 			$tempString = str_replace("%3A",":",$tempString);	
 			$tempString = explode("=", $tempString)[4];
-			
-			$data[$key]['ownUrl'] = $tempString;
+			$listOfUrls [] = $tempString;
 		}
-		return $data;
+		$_SESSION['data']['urls'] = $listOfUrls;
 	}
 	
 	function asyncURLReader($listOfUrls) {
