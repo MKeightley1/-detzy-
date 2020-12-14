7b099879ba87ac529d5af906f51e686b26d3eb84
Maurice Keightley <maurice.keightley@gmail.com>
2020-12-14 22:05:25 +1000

    jbj

--- versions/14122020/index.php
+++ versions/14122020/index.php
@@ -1,8 +1,8 @@
 <?php	
 	
 	if((int)$_GET['data'][1]){
-		echo json_encode($_GET);
+		echo json_encode($_GET['data']);
 	}else{
-		echo json_encode($_POST);
+		echo json_encode($_POST['data']);
 	}
 	
